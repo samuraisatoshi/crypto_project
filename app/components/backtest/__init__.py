e99bf83f562a8a6f73a4a4be020e329b2d000893
Decentralized Apps <samuraisatoshi@cryptoworld.8shield.net>
2025-01-05 19:05:24 -0300

    Create __init__.py
    FIrst commit

---
+++ app/components/backtest/__init__.py
@@ -0,0 +1,10 @@
+"""
+Backtest components initialization.
+"""
+from .chart_component import ChartComponent
+from .results_display import ResultsDisplay
+
+__all__ = [
+    'ChartComponent',
+    'ResultsDisplay'
+]
